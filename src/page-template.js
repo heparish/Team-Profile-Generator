9afe44f27e79f18b042de44ad2e6fa3cd5fcacdd
Haley Parish <heparish@ucdavis.edu>
2021-06-06 20:13:25 -0700

    template tweak

--- src/page-template.js
+++ src/page-template.js
@@ -68,7 +68,7 @@ const generatePage = templateData => {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.1/css/bulma.min.css" />
     </head>
     <body>
-        <section class="hero is-primary is-bold">
+        <section class="hero hero is-success is-bold">
             <div class="hero-body">
                 <div class="container">
                     <h1 class="title">My Team</h1>
